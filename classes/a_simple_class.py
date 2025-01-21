5b286f8400db5bd5017b33e4f4e9874869a77d68
echalmers <echalmers@mtroyal.ca>
2025-01-21 09:47:53 -0700

    classes examples

--- classes/a_simple_class.py
+++ classes/a_simple_class.py
@@ -1,13 +1,23 @@
 
 class Character:  # convention is camel case for class names
     
-    def __init__(self, character_name: str, character_hp: int) -> None:
+    def __init__(self, character_name: str, character_hp: int, damage: int) -> None:
         self.name = character_name
         self.hit_points = character_hp
+        self.damage = damage
 
-
-my_character = Character('eric', 5)
-my_other_character = Character('Daniel', 1)
+my_character = Character('eric', 5, 1)
+my_other_character = Character('Daniel', 1, 1_000_000)
 print(f'{my_character.name} has {my_character.hit_points} hp')
 print(f'{my_other_character.name} has {my_other_character.hit_points} hp')
+
+def attack(attacker: Character, defender: Character) -> None:
+    # hp_remaining = defender.hit_points - attacker.damage
+    # defender.hit_points = hp_remaining
+
+    # more concise
+    defender.hit_points -= attacker.damage
+
+attack(my_other_character, my_character)
+print(my_character.hit_points)
 
--- classes/car.py
+++ classes/car.py
@@ -1,15 +1,29 @@
+class Person:
+
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
 class Car:
 
-    def __init__(self, num_seats:int, car_type: str, model: str, odo: float) -> None:
+    def __init__(self, num_seats:int, car_type: str, model: str, odo: float, owner: Person) -> None:
         self.num_seats = num_seats
         self.car_type = car_type
         self.model = model
         self.odometer = odo
+        self.owner = owner
+
+
+
+
+
+eric = Person('eric', 'echalmers@mtroyal.ca')
+
 
 fleet = [
-    Car(3, 'truck', 'sierra', 500_000),
-    Car(2, 'sports car', 'stingray', 3),
-    Car(10, 'van', 'large van', 1000)
+    Car(3, 'truck', 'sierra', 500_000, eric),
+    Car(2, 'sports car', 'stingray', 3, eric),
+    Car(10, 'van', 'large van', 1000, eric)
 ]
 
 for car in fleet:
